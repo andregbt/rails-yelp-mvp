141f9343939e4f5e9f27ce2d30b60e3e4efad256
andre teixeira <andre.gomes.teixeira22@gmail.com>
2022-11-23 15:03:27 +0000

    Yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,13 @@
+class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  has_many :reviews, dependent: :destroy
+end
+
+
+
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.
